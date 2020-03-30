0b5bcc5a6c99f9b78b2dd9b0938bd3b6ebff5ce8
DeSireFire <1025212779@qq.com>
2020-03-30 09:59:02 +0800

    Vue和Jquery是解决不同问题的，不应该让它们对立

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # VueCli4-JQueryExample
-一个用VueCli4+JQuery的配置案例，方便日后查阅配置方法。（因为VueCli3的config不见了..）
+一个用VueCli4 + JQuery的配置案例，方便日后查阅配置方法。（因为VueCli3的config不见了..）
---
+++ VueCli4-JQueryExample/src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p><a href="http://www.w3school.com.cn" id="w3s">W3School.com.cn</a></p>
+    <button @click="jqueryDemo">显示 href 值</button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import $ from 'jquery';
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  },
+  methods:{
+    jqueryDemo() {
+      console.log('2333')
+      alert($("#w3s").attr("href"));
+    }
+  },
+}
+</script>
